47b972bf69b046e91f4c7e2e3dd963e9c5058fcb
John Doe <johndoe@example.com>
2019-07-02 14:05:07 +0200

    help

--- app.py
+++ app.py
@@ -56,16 +56,11 @@ def get_api_data():
     resp.status_code = 200
     return resp
 
-@app.route('/post_api_data/<uuid>', methods = ['POST'])
+@app.route('/post_api_data/<uuid>', methods = ['GET', 'POST'])
 def post_api_data(uuid):
     input_data = request.get_json()
     print (input_data)
     return uuid
-
-
-@app.route('/query', methods = ['GET'])
-def get_all_users():
-    return ApiData.query.all()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
