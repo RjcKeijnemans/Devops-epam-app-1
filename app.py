dfc6052d4f689e42432c2479a51e33dba85a2317
John Doe <johndoe@example.com>
2019-07-02 14:47:01 +0200

    help

--- app.py
+++ app.py
@@ -56,10 +56,15 @@ def get_api_data():
     resp.status_code = 200
     return resp
 
-@app.route("/post_api_data/<id>", methods=['GET'])
+
 def post_api_data(id):
     entry = db_session.query(ApiData).get(id)
     return entry
+
+@app.route("/query_api_data", methods=['GET'])
+def query_api_data():
+    resp = jsonify(json_list=[i.serialize for i in post_api_data()])
+    return resp   
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
