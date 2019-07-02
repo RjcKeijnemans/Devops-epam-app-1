6a00da93ff586f8c2a22d082511a7b98e827ccb8
John Doe <johndoe@example.com>
2019-07-02 15:20:39 +0200

    Post-delete integration

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def get_db_api_data() -> ApiData:
 
 @app.route("/", methods=["GET"])
 def app_index():
-    return "Available methods: get_api_data, add_api_data/values1, values2, values3"
+    return "Available methods: get_api_data, add_api_data/'values1, values2, values3', delete_api_data/'id'"
 
 
 @app.route("/get_api_data", methods=["GET"])
@@ -61,7 +61,14 @@ def add_api_data(values1, values2, values3):
     insert = ApiData(uuid1=values1, uuid2=values2, uuid3=values3)
     db_session.add(insert)
     db_session.commit()
-    return 'Succesfully created a new id and inserted the values into the ApiData-table'
+    return 'Succesfully created a new id and inserted the values into the database table'
+
+@app.route("/delete_api_data/<id1>")
+def delete_api_data(id1):
+    insert = ApiData(id=id1)
+    db_session.delete(insert)
+    db_session.commit()
+    return 'Succesfully deleted the id from the database table'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
