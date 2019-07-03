80016b5978f9f32c02362a769b37c4ff41392995
John Doe <johndoe@example.com>
2019-07-03 14:16:42 +0200

    Cleanup + LB option

--- sql/db_initialization.sh
+++ sql/db_initialization.sh
@@ -1,7 +1,7 @@
 az account set --subscription 94fbadf8-645b-426b-8455-4de8b5a74bb5
 
 # Set resourcegroup and location
-resourceGroupName=Flask-resourcegroup-
+resourceGroupName=Flask-resourcegroup-$RANDOM
 location=westeurope
 
 # Set an admin login and password for your database
@@ -16,14 +16,15 @@ DBURL="server-10002.postgres.database.azure.com"
 startip=0.0.0.0
 endip=0.0.0.0
 
-# Set name of App Service Plan
-plan=scale_out_plan
+# )Optional) Set name of App Service Plan and Web App
+planName=SampleAppServicePlan
+webappname=Flask-EPAM-Test-App-$RANDOM
+
+# (Optional) Set Repository URL
+repoUrl= https://github.com/Xangliev/Devops-epam-app.git
 
 # Create a resource group
 az group create --name $resourceGroupName --location $location
-
-# Create an app service plan in the resource group
-az group deployment create --name $plan --resource-group $resourceGroupName --template-uri https://raw.githubusercontent.com/Xangliev/Devops-epam-app/master/service_plan.json
 
 # Create a logical server in the resource group
 az postgres server create --name $servername --resource-group $resourceGroupName --location $location --admin-user $adminlogin --admin-password $password --sku-name B_Gen5_1
@@ -36,3 +37,11 @@ PGPASSWORD=$password psql -h $DBURL -U $adminlogin@$servername --file='db.sql' p
 
 # Connect to new database with user credentials and insert inital values
 PGPASSWORD='Interforaewg098!' psql -h $DBURL -U 'api_db_user'@$servername -d 'api_db' --file='db1.sql'
+
+# Steps for deploying web app, with load balancing and CD from Github Source enabled
+# Create webapp from local git
+# az webapp up -n $webappname -g $resourceGroupName -l $location --plan $planName
+# Enable CD from Repository
+# az webapp deployment source config --repo-url $repoUrl --resource-group $resourceGroupName --name $webappname
+# Enable Load Balancing
+# az appservice plan update --number-of-workers 2 --resource-group $resourceGroupName --name $planName
