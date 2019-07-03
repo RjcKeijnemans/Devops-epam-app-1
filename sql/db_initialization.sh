dde888230b7d7715876fc18b49c476a199ac5557
John Doe <johndoe@example.com>
2019-07-03 10:56:27 +0200

    Fixed

--- sql/db_initialization.sh
+++ sql/db_initialization.sh
@@ -2,7 +2,7 @@ az account set --subscription 94fbadf8-645b-426b-8455-4de8b5a74bb5
 
 # Set resourcegroup and location
 resourceGroupName=Flask-resourcegroup-
-location=centralus
+location=westeurope
 
 # Set an admin login and password for your database
 adminlogin=SqlAdmin
@@ -16,11 +16,14 @@ DBURL="server-10002.postgres.database.azure.com"
 startip=0.0.0.0
 endip=0.0.0.0
 
+# Set name of App Service Plan
+plan=scale_out_plan
+
 # Create a resource group
 az group create --name $resourceGroupName --location $location
 
 # Create an app service plan in the resource group
-az group deployment create --resource-group $resourceGroupName --template-uri https://raw.githubusercontent.com/Xangliev/Devops-epam-app/master/service_plan.json
+az group deployment create --name $plan --resource-group $resourceGroupName --template-uri https://raw.githubusercontent.com/Xangliev/Devops-epam-app/master/service_plan.json
 
 # Create a logical server in the resource group
 az postgres server create --name $servername --resource-group $resourceGroupName --location $location --admin-user $adminlogin --admin-password $password --sku-name B_Gen5_1
