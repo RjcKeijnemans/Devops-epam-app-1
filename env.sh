cd5a7e65ce37c2afbc32bcb23d688da6c5c9f5ab
John Doe <johndoe@example.com>
2019-07-04 15:27:02 +0200

    Cleanup the whole repo

--- env.sh
+++
@@ -1,5 +0,0 @@
-export POSTGRES_USER=api_db_user
-export POSTGRES_PASSWORD=Interforaewg098!
-export POSTGRES_DB=api_db
-export SUBDOMAIN_SERVER=server-10002.postgres.database.azure.com
-export DB_PORT=5432
