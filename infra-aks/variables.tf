d56183452d307763d5730a25ddfcc390eef8485f
John Doe <johndoe@example.com>
2019-07-15 10:51:11 +0200

    Add k8

---
+++ infra-aks/variables.tf
@@ -0,0 +1,49 @@
+# Secret Credentials //  Use Terraform.tfvars File
+variable "subscription_id" {
+  type = "string"
+  default = "x"
+}
+variable "client_id" {
+  type = "string"
+  default = "x"
+}
+variable "client_secret" {
+  type = "string"
+  default = "x"
+}
+variable "tenant_id" {
+  type = "string"
+  default = "x"
+}
+
+# Standard Variables:
+variable "resource_group_name" {
+  type = "string"
+  default = "Flask-resourcegroup-epam-name-test"
+}
+variable "location" {
+  type = "string"
+  default = "centralus"
+}
+variable "server_name" {
+  type = "string"
+  default = "server-20075"
+}
+variable "admin_name" {
+  type = "string"
+  default = "Sqladmin"
+}
+variable "admin_pass" {
+  type = "string"
+  default = "ChangeYourAdminPassword1"
+}
+
+variable "kluster_name" {
+  type = "string"
+  default = "flask-aks"
+}
+
+# Output Variables
+output "database_endpoint" {
+  value = "${var.server_name}.postgres.database.azure.com"
+}
